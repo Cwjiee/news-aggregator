e23dcd846437ec44804a48b02aded177f84d0848
weijie <jackchong398@gmail.com>
2025-03-26 14:26:55 +0800

    update

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,9 +1,9 @@
-require 'open-uri'
-require 'nokogiri'
+require "open-uri"
+require "nokogiri"
 
 class NewsController < ApplicationController
   def index
-    @years = (2022..2025).to_a 
+    @years = (2022..2025).to_a
   end
 
   def months
@@ -12,7 +12,6 @@ class NewsController < ApplicationController
   end
 
   def monthly_news
-
     @year = params[:year] || Time.now.year
     @month = params[:month] || Time.now.month
     page = params[:page] || 1
@@ -38,41 +37,39 @@ class NewsController < ApplicationController
           logger.info("title #{link}")
           next if title.blank?
 
-          all_news << {title: title, url: link['href'], time: time}
+          all_news << { title: title, url: link["href"], time: time }
         end
       end
-
     end
 
     @news = Kaminari.paginate_array(all_news).page(params[:page]).per(20)
   end
 
-#   def fetch_titles
-#     year = params[:year] || Time.now.year
-#     month = params[:month] || Time.now.month
-#     page = params[:page] || 1
-# 
-#     link = "https://www.theverge.com/archives"
-# 
-#     @news = []
-# 
-#     # (2022..2025).each do |year|
-#     #   (1..12).each do |month|
-#     #     (1..15).each do |page|
-#     url = "#{link}/#{year}/#{month}/#{page}"
-#     logger.info("scraping #{url}")
-#     doc = Nokogiri::HTML(URI.open(url))
-# 
-#     doc.css("a#_1lkmsmo1").each do |link|
-#       title = link.text.strip
-#       next if title.blank?
-# 
-#       @news << {title: title, url: link['href']}
-#       # News.find_or_create_by(title: title, url: link['href'])
-#     end
-#     #     end
-#     #   end
-#     # end
-#   end
+  #   def fetch_titles
+  #     year = params[:year] || Time.now.year
+  #     month = params[:month] || Time.now.month
+  #     page = params[:page] || 1
+  #
+  #     link = "https://www.theverge.com/archives"
+  #
+  #     @news = []
+  #
+  #     # (2022..2025).each do |year|
+  #     #   (1..12).each do |month|
+  #     #     (1..15).each do |page|
+  #     url = "#{link}/#{year}/#{month}/#{page}"
+  #     logger.info("scraping #{url}")
+  #     doc = Nokogiri::HTML(URI.open(url))
+  #
+  #     doc.css("a#_1lkmsmo1").each do |link|
+  #       title = link.text.strip
+  #       next if title.blank?
+  #
+  #       @news << {title: title, url: link['href']}
+  #       # News.find_or_create_by(title: title, url: link['href'])
+  #     end
+  #     #     end
+  #     #   end
+  #     # end
+  #   end
 end
-
